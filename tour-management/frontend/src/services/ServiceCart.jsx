6a3abecd07de1d6ce1e3213ab0ff7d7a86f8147e
Chetan Sanjay Shivade <chetanshivade34@gmail.com>
2023-07-24 17:56:01 +0530

    first commit

--- tour-management/frontend/src/services/ServiceCart.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const ServiceCart = () => {
-  return <div>ServiceCart</div>;
-};
--- tour-management/frontend/src/services/ServiceList.jsx
+++ tour-management/frontend/src/services/ServiceList.jsx
@@ -1,4 +1,11 @@
 import React from "react";
+import ServiceCard from "./ServiceCard";
+import { Col } from "reactstrap";
+
+import weatherImg from "../assets/images/weather.png";
+import guideImg from "../assets/images/guide.png";
+import weatherImg from "../assets/images/weather.png";
+import weatherImg from "../assets/images/weather.png";
 
 const ServiceList = () => {
   return <div>ServiceList</div>;
